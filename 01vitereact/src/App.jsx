5cbea51229dfde06d1bb000b68e70b1dbd1b0125
Ka1yani <kalyanijamodkar3@gmail.com>
2023-12-29 17:54:21 +0530

    First vitereact code

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+import First from "./first"
+function App() {
+
+  return (
+    //<h1>Hello react with vite</h1>
+    <First/>
+
+  )
+}
+
+export default App
---
+++ 01vitereact/src/first.jsx
@@ -0,0 +1,17 @@
+const f1 = First => {return <h3> This my first file in viteode.</h3>}
+export default f1;
+
+/* Note: Adj JSX element/ javascript must be wrapped in enclosing tag (only one tab can pass 
+through jsx element) 
+
+Sol: You can use JSX fragments ( <> ......</>)
+ */
+
+/* function First () {
+    return (
+    <h3>This my first file in viteode.</h3>
+    )
+}
+
+export default First; */
+
