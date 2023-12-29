31fa8733004bed8e568fd2c547401b1ceb508d20
Ka1yani <kalyanijamodkar3@gmail.com>
2023-12-29 17:41:25 +0530

    First react code

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import First from './First'
+
+function App() {
+  return (
+    <>
+    <First />
+    <h1>Hello React</h1>
+    </> 
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/First.js
@@ -0,0 +1,10 @@
+const f2 = First => {return (<h1>This my First code in react</h1>)}
+export default f2;
+
+/* function First () {
+    return (
+    <h1>This my First code in react</h1>
+    ) 
+} 
+
+export default First; */
